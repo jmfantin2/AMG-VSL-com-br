f331d8aff8da2822acd22f7606cd386f423c5b12
JM FANTIN <jmfantin@outlook.com>
2024-12-10 07:44:25 -0300

    Update Countdown.tsx

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,7 +2,7 @@ import Countdown, { CountdownRendererFn, zeroPad } from 'react-countdown';
 
 const CountdownTimer = () => {
   // Set the countdown date to Oct 5, 2024
-  const countdownDate = new Date('2024-12-11T00:00:00');
+  const countdownDate = new Date('2024-12-18T00:00:00');
 
   // Renderer function with proper typing for the props
   const renderer: CountdownRendererFn = ({ days, hours, minutes, seconds }) => {
