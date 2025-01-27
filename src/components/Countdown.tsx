9e143217903d90f8fcee8b10d34011c2fd24b179
JM Fantin <jmfantin@outlook.com>
2025-01-27 11:12:34 -0300

    update countdownDate timer mechanism, using seedDate and range

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,16 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import Countdown, { CountdownRendererFn, zeroPad } from 'react-countdown';
 
 const CountdownTimer = () => {
-  // Set the countdown date to Oct 5, 2024
-  const countdownDate = new Date('2024-10-05T00:00:00');
+  const rangeInMilliseconds = 5 * 24 * 60 * 60 * 1000; // 5 days in milliseconds
+  const [countdownDate, setCountdownDate] = useState<Date | null>(null);
 
-  // Renderer function with proper typing for the props
+  useEffect(() => {
+    // Seed date to initialize the countdown cycle
+    const seedDate = new Date('2025-01-26T00:00:00');
+    const now = new Date();
+    const diff = now.getTime() - seedDate.getTime();
+    const cyclesSinceSeed = Math.floor(diff / rangeInMilliseconds); // Number of cycles passed since seedDate
+    const nextCycleStart =
+      seedDate.getTime() + (cyclesSinceSeed + 1) * rangeInMilliseconds;
+
+    setCountdownDate(new Date(nextCycleStart));
+  }, [rangeInMilliseconds]);
+
+  if (!countdownDate) {
+    return <div>Carregando...</div>; // Render a loading state while the date is being calculated
+  }
+
+  // Renderer function for the countdown timer
   const renderer: CountdownRendererFn = ({ days, hours, minutes, seconds }) => {
-    // Calculate total hours by converting days to hours
-
     return (
       <div className="text-center bg-clip-text text-transparent bg-gradient-to-br from-[#E1D48F] to-[#D1A84E] text-4xl font-mono font-bold">
-        <h1 className="text-center text-xl font-bold ">
+        <h1 className="text-center text-xl font-bold">
           Cupom de desconto termina em
         </h1>
         <h2 className="text-4xl">
